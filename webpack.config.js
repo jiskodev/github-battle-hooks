5ce154ec3663922c7343a0249c3127b80fbec629
jiskodev <sergio.jisko@gmail.com>
2020-10-30 17:11:06 +0100

    fixed browser routing

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
+
 
 module.exports = {
   entry: './app/index.js',
@@ -18,7 +20,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
-    })
+    }),
+    new CopyPlugin({ patterns: [{ from : '_redirects' }] })
   ],
   devServer: {
     historyApiFallback: true
