262ff6f45ccc324590e1fc6240287fb038fa6276
jiskodev <sergio.jisko@gmail.com>
2020-11-04 07:01:28 +0100

    Card and Nav - Theme useContext

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,32 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { ThemeConsumer } from '../contexts/theme'
+import ThemeContext from '../contexts/theme'
 
 {/* REUSABLE CARD FOR RESULTS AND REPOS */}
 
 export default function Card ({ header, subheader, avatar, href, name, children }) {
+    const theme = React.useContext(ThemeContext)
+
     return (
-        <ThemeConsumer>
-            {(theme) => (
-                <div className={`card bg-${theme}`}>
-                    <h4 className="header-lg center-text">
-                        {header}
-                    </h4>
-                    <img src={avatar} alt={`Avatar for ${name}`} className="avatar"/>
-                    {subheader && (
-                    <h4 className="center-text">
-                        {subheader}
-                    </h4>
-                    ) }
-                    <h2 className="center-text">
-                        <a href={href} className="link">{name}</a>
-                    </h2>
-                    {children}
-                </div>
-            )}
-        </ThemeConsumer>
-            
-            
+    <div className={`card bg-${theme}`}>
+        <h4 className="header-lg center-text">
+            {header}
+        </h4>
+        <img src={avatar} alt={`Avatar for ${name}`} className="avatar"/>
+        {subheader && (
+        <h4 className="center-text">
+            {subheader}
+        </h4>
+        ) }
+        <h2 className="center-text">
+            <a href={href} className="link">{name}</a>
+        </h2>
+        {children}
+    </div>
     )
 }
 
